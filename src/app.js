a318512bf405eb95c1865376224b5ad5660b919b
chdineshbabu <chdinesh4128@gmail.com>
2024-10-18 17:42:31 +0530

    Utils Done

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import e from "express";
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+const app = e()
+app.use(cors())
+app.use(e.json({limit: '16kb'}))
+app.use(e.urlencoded({extended:true, limit:'16kb'}))
+app.use(e.static('public'))
+app.use(cookieParser())
+
+
+export {app} 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
-
-
-
-
+import { app } from './app.js';
 dotenv.config({
     path:'./env'
 })
-
-connectDB() 
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Running of port ${process.env.PORT || 8000}`)
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB Connection failed",error)
+}) 
